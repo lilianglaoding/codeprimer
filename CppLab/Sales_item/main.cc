48a83ba7ea527538a1911d281dfb8d3f527967d8
lilianglaoding <liliang8904@126.com>
2014-04-15 22:46:21 +0800

    first commit

--- CppLab/Sales_item/main.cc
+++ CppLab/Sales_item/main.cc
@@ -1,15 +1,7 @@
-#include <iostream>
-#include "Sales_item.h"
+#include "Sales_item"
 
 int main()
 {
-  Sales_item item("0-201-82470-1");
-  item.display(std::cout);
-  Sales_item item1(std::cin);
-  item1.display(std::cout);
-  Sales_item *p = new Sales_item();
-  p->display(std::cout);
-  Sales_item *p1 = new Sales_item;
-  p1->display(std::cout);
+  Sales_item item;
   return 0;
 }
