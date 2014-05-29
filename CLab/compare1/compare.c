f00227a168a9a22d8030305539dfdc0246b72e0e
lilianglaoding <liliang8904@126.com>
2014-05-29 23:20:37 +0800

    first commit

--- CLab/compare1/compare.c
+++ CLab/compare1/compare.c
@@ -6,11 +6,21 @@ int compare(int *a, int *b, int m, int n)
 {
   int i = 0;
   int j;
+  int ms = 0;
+  int ns = 0;
   int A[MAXSIZE], B[MAXSIZE];
   while (i < m && i < n && a[i] == b[i])
     ++i;
-  for (j = i; j < m && j < n; ++j)
+  for (j = i; j < m; ++j)
   {
-    if (a[j] - b[i])
+    A[j - i] = a[j];
+    ++ms;
   }
+  for (j = i; j < n; ++j)
+  {  
+    B[j - i] = b[j];
+    ++ns;
+  }
+  if (ms == ns && ms == 0)
+    return 0;
 }
