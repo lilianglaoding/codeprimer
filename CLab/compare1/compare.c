aaa826b2f9fad7d3846ba3398c356320bdc8ef0e
lilianglaoding <liliang8904@126.com>
2014-05-30 21:12:56 +0800

    first commit

--- CLab/compare1/compare.c
+++ CLab/compare1/compare.c
@@ -23,4 +23,19 @@ int compare(int *a, int *b, int m, int n)
   }
   if (ms == ns && ms == 0)
     return 0;
+  printf("ms: %d\n", ms);
+  printf("ns: %d\n", ns);
+  //if (ms == 0 && ns > 0 || ms > 0 && ns > 0 && A[0] < B[0])
+  if (ms == 0 && ns > 0 || ms > 0 && ns > 0 && A[0] < B[0])
+    return -1;
+  else
+    return 1;
+}
+
+int main()
+{
+  int a[7] = {0, 1, 2, 3, 4, 4, 5};
+  int b[MAXSIZE] = {0, 1, 2, 3, 4, 5};
+  int comp = compare(a, b, 7, MAXSIZE);
+  printf("%d\n", comp);
 }
