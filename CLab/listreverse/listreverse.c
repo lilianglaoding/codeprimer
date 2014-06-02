10d6d7e9ca446b69ca4f5348a4a35b5db9e279f4
lilianglaoding <liliang8904@126.com>
2014-06-02 10:19:28 +0800

    first commit

--- CLab/listreverse/listreverse.c
+++ CLab/listreverse/listreverse.c
@@ -101,30 +101,72 @@ void Delete_Repeat(LinkList head)
   }
 }
 
+LinkList Merge_LinkList(LinkList head1, LinkList head2)
+{
+  LinkList p1 = head1->next;
+  LinkList p2 = head2->next;
+  LinkList ptemp, rear;
+  free(head2);
+  head1->next = NULL;
+  rear = head1;
+  while (p1!= NULL && p2 != NULL)
+  {
+    if (p1->data < p2->data)
+    {
+      ptmep = p1;
+      p1->next = ptemp->next;
+      head1->next = ptemp;
+      ptemp->next = head1->next;
+    }
+    else
+    {
+      ptemp = p2;
+      p2->next = ptemp->next;
+      head1->next = ptemp;
+      ptemp->next = head1->next;
+    }
+  }
+  return head1;
+}
+
 int main()
 {
   LinkList head;
+  LinkList head2;
   head = Init_LinkList();
+  head2 = Init_LinkList();
   for (int i = 0; i < 4; ++i)
   {
     Insert_LinkList(head, i);
     Insert_LinkList(head, i);
+    Insert_LinkList(head2, i);
+    Insert_LinkList(head2, i);
   }
   printf("\n----------before reverse----------\n");
   Print_LinkList(head);
+  printf("\n");
+  Print_LinkList(head2);
   Reverse_LinkList(head);
+  Reverse_LinkList(head2);
   printf("\n-----after reverse-----\n");
   Print_LinkList(head);
-  Reverse_LinkList1(head);
-  printf("\n--------after anoter reverse------\n");
-  Print_LinkList(head);
-  if (head->next == NULL)
-    printf("head->next == NULL\n");
+  printf("\n");
+  Print_LinkList(head2);
+  //Reverse_LinkList1(head);
+  //printf("\n--------after anoter reverse------\n");
+  //Print_LinkList(head);
+  //if (head->next == NULL)
+  //  printf("head->next == NULL\n");
   Delete_Repeat(head);
-  Delete_Repeat(head);
+  Delete_Repeat(head2);
   printf("\n--------ater delete repeat--------\n");
   Print_LinkList(head);
   printf("\n");
+  Print_LinkList(head2);
+  printf("\n");
+  head = Merge_LinkList(head, head2);
+  printf("\n---------after merge--------------\n");
+  Print_LinkList(head);
   Destroy_LinkList(head);
 
   return 0;
