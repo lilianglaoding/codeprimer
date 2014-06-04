4ae0be688c32909ce5a930cc3b9ad51228b711ef
lilianglaoding <liliang8904@126.com>
2014-06-04 23:40:28 +0800

    first commit

--- CLab/Stack/maze/maze.c
+++ CLab/Stack/maze/maze.c
@@ -3,40 +3,42 @@
 #include "maze.h"
 #include "Stack.h"
 
+//if find a path, return 1, otherwise, return 0;
 int path(int maze[m][n], item move[8])
 {
   SeqStack *s;
   s = Init_Stack();
+  
+  int x, y, d;
+  int i, j;     //current position
   datatype temp;
-  int x, y ,d, i, j;
-  temp.x = 1;
-  temp.y = 1;
-  type.d = -1;
-  
+  x = y = 1;
+  d = -1;
+  i = j = 1;
+  temp.x = x;
+  temp.y = y;
+  temp.d = d;
+  d++;
   Push_Stack(s, temp);
-  
   while (!Empty_Stack(s))
   {
-    temp = Top_Stack(s);
-    Pop_Stack(s);
+    Pop_Stack(s, &temp);
     x = temp.x;
     y = temp.y;
-    d = temp.d + 1;
-    
+    d = temp.d;
+    d++;
     while (d < 8)
     {
       i = x + move[d].x;
       j = y + move[d].y;
       if (maze[i][j] == 0)
       {
-	temp = {x, y, d};
+	temp.x = i;
+	temp.j = j;
+	temp.d = d;
 	Push_Stack(s, temp);
-	
-	x = i;
-	y = i;
 	maze[i][j] = -1;
-	
-	if (x == m && y == n)
+	if (i == m && j == n)
 	  return 1;
 	else
 	  d = 0;
@@ -47,3 +49,4 @@ int path(int maze[m][n], item move[8])
   }
   return 0;
 }
+  
