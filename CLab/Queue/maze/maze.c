1f75341c6897d2fd911b94f829797165b0321070
lilianglaoding <liliang8904@126.com>
2014-06-09 23:09:03 +0800

    shortest way in maze

--- CLab/Queue/maze/maze.c
+++ CLab/Queue/maze/maze.c
@@ -4,7 +4,47 @@
 
 //find a shortest way in maze
 
-int maze(int maze[][], int m, int n)
+int maze(int maze[][], int move[8])
 {
-  
+  int i, j, x, y, d;
+  i = 1;
+  j = 1;
+  x = 1;
+  j = 1;
+  sq[0].x = 1;
+  sq[0].y = 1;
+  sq[0].pre = -1;
+  front = -1;
+  rear = 0;
+  SqType temp;
+  temp.x = 1;
+  temp.y = 1;
+  temp.pre = -1;
+  In_Queue(sq, temp);
+  while (front <= rear)
+  {
+    Out_Queue(sq, &temp);
+    x = temp.x;
+    y = temp.y;
+    for (int v = 0; v < 8; ++v)
+    {
+      x = x + move[v].x;
+      y = y + move[v].y;
+      temp.x = x;
+      temp.y = y;
+      temp.pre = front;
+      if (maze[x][y] == 0)
+      {
+	In_Queue(sq, temp);
+	maze[x][y] = -1;
+      }
+      if (x == m && y == n)
+      {
+	print_path(sq);
+	restore_maze(maze[][]);
+	return 1;
+      }
+    } // end for
+    ++front;
+  } // end while
 }
