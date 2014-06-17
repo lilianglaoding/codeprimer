dcc7de310ad2dd253151050440a2d44b7b8282e5
lilianglaoding <liliang8904@126.com>
2014-06-17 23:08:45 +0800

    linkmatrix

--- CLab/Matrix/linkmatrix/linkmatrix.c
+++ CLab/Matrix/linkmatrix/linkmatrix.c
@@ -55,6 +55,17 @@ MLink CreateMlink()
   return H;
 }
 
+MLink FindColPtr(MLink H, int col)
+{
+  MLink p = H->v_next.next;  //p points to first row pointer
+  i = 1;
+  
+  while (i++ < col)
+    p = p->v_next.next;
+
+  return p;
+}
+
 MLink AddMlink(MLink Ha, MLink Hb)
 {
   if (Ha->row != Hb->row || Ha->col != Hb->col)
@@ -63,12 +74,58 @@ MLink AddMlink(MLink Ha, MLink Hb)
   MLink pa = Ha->v_next.next;   //pa points to row 1 of a
   MLink pb = Hb->v_next.next;   //pb points to row 1 of b
   
-  MLink pa, pb, qa, qb;
+  MLink pa, pb, qa, p;
   ca = pa;
   cb = pb;
-  pa = pa->right;
-  pb = pb->right;
-  while (pb->col != 0)
-}
 
+  while (pb->col != 0)
+  {
+    ca = pa;
+    cb = pb;
+    qa = pa;
+    pa = pa->right;    //points to first node of row
+    pb = pb->right;    //points to first node of row
+    
+    for ( ; pb->right != cb; pb = pb->right)
+    {
+      if (pa->col < pb->col)
+      {
+	qa = pa;
+	pa = pa->right;
+      }
+      else if (pa->col > pb->col)
+      {
+	p = (MNode *)malloc(sizeof(MNode));
+	p->row = pb->row;
+	p->col = pb->col;
+	p->v_next.v = pb->v_next.v;
+	p->right = qa->right;
+	qa->right = p;
+	qa = FindColPtr(Ha, pb->col);
+	while (qa->down->row < pb->row && qa->down != qa)
+	  qa = qa->down;
+	p->down = qa->down;
+	qa->down = p;
+      }
+      else
+      {
+	int x = pa->v_next.v + pb->v_next.v;
+	if (x != 0)
+	  pa->v_next.v += pb->v_next.v;
+	else
+	{
+	  qa->right = pa->right;
+	  qa = FindColPtr(Ha, pb->row);
+	  while (qa->down->row < pb->row && qa->down != qa)
+	    qa = qa->down;
+	  qa->down = p->down;
+	  free(p);
+	}
+      }
+    }
+    pa = pa->v_next.next;
+    pb = pb->v_next.next;
+  }
+  return Ha;
+}
 
