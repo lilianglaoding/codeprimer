82b485e6d28bd6c71420fcd1dfed7052c09f423f
lilianglaoding <liliang8904@126.com>
2014-06-18 22:55:52 +0800

    general lists

--- CLab/Matrix/glist/glist.c
+++ CLab/Matrix/glist/glist.c
@@ -5,5 +5,21 @@
 
 GList Head(GList ls)
 {
+  if (ls != NULL)
+    return ls->hp;
+  else
+    return NULL;
+}
+
+GList Head(GList ls)
+{
+  if (ls != NULL)
+    return ls->tp;
+  else
+    return NULL;
+}
+
+int Create(GList *ls, char *S)
+{
   
 }
