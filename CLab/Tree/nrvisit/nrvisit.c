3da93a961a8c2ef659865176a9f87da6c308a5f4
lilianglaoding <liliang8904@126.com>
2014-06-24 22:56:17 +0800

    nrvist

--- CLab/Tree/nrvisit/nrvisit.c
+++ CLab/Tree/nrvisit/nrvisit.c
@@ -5,4 +5,41 @@
 
 #define MAXSIZE 2048
 
+void NRPreOrder(BiTree bt)
+{
+  if (bt == NULL)
+    return ;
+  BiTree p;
+  BiTree stack[MAXSIZE];
+  p = bt;
+  int top;
+  top = 0;
+  while (top != 0)
+  {
+    while (p != NULL)
+    {
+      visit(p->data);
+      if (top <= MAXSIZE - 1)
+      {
+	stack[top] = p;
+	top++;
+	p = p->lchild;
+      }
+      else
+      {
+	printf("stack is full\n");
+	return ;
+      }
+    }
+    if (top <= 0)
+      return ;
+    else
+    {
+      top--;
+      p = stack[top];
+      p = p->rchild;
+    }
+  }
+}
+
 
