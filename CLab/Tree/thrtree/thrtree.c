056d501a25ca1d52f7f3dbfff3bfff2a4476f787
lilianglaoding <liliang8904@126.com>
2014-06-30 23:07:05 +0800

    thread tree

--- CLab/Tree/thrtree/thrtree.c
+++ CLab/Tree/thrtree/thrtree.c
@@ -49,3 +49,28 @@ void InOrderThreading(BiThrTree T)
     InOrder(T->rchild);
   }
 }
+
+BiThrTree InPreNode(BiThrTree p)
+{
+  BiThrTree pre;
+  pre = p->lchild;
+  if (pre->ltag != 1)
+  {
+    while (pre->rtag != 1)
+      pre = pre->rchild;
+  }
+  return pre;
+}
+
+BiThrTree InPostNode(BiThrTree p)
+{
+  BiThrTree post;
+  post = p->rchild;
+  if (post->rtag != 1)
+  {
+    while (post->ltag != 1)
+      post = post->lchild;
+  }
+  return post;
+}
+
