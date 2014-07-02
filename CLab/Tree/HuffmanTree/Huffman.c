3189b0fa27e58414acd6ce80df6b9f22071ae7e9
lilianglaoding <liliang8904@126.com>
2014-07-03 07:44:03 +0800

    Huffman Tree

--- CLab/Tree/HuffmanTree/Huffman.c
+++ CLab/Tree/HuffmanTree/Huffman.c
@@ -3,4 +3,26 @@
 
 #include "Huffman.h"
 
+void HuffmanTree(HNodeType HuffNode[])
+{
+  int i, j;
+  int m1, m2, x1, x2;
+  int n;
 
+  scanf("%d", &n);
+
+  for (i = 0; i < 2 * n; ++i)
+  {
+    HuffNode[i].weight = 0;
+    HuffNode[i].parent = -1;
+    HuffNode[i].lchild = -1;
+    HuffNode[i].rchild = -1;
+  }
+  
+  for (i = 0; i < n; ++i)
+  {
+    HuffNode[i].weight;
+  }
+
+  
+}
