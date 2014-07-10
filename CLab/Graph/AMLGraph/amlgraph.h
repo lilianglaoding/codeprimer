245ac7295b107fce7e8f768e1ef9a094145e6f2b
lilianglaoding <liliang8904@126.com>
2014-07-10 23:19:24 +0800

    adjacency multilist

--- CLab/Graph/AMLGraph/amlgraph.h
+++ CLab/Graph/AMLGraph/amlgraph.h
@@ -1,6 +1,31 @@
 #ifndef AMLGRAPH_H
 #define AMLGRAPH_H
 
+#define MAXVERTEXNUM 100
 
+typedef char VertexType;
+typedef int EdgeType;
+typedef int InfoType;
+typedef int VisitIf;
+
+typedef struct EBox
+{
+  VisitIf mark;
+  int ivex, jvex;
+  struct EBox *ilink, *jlink;
+  InfoType info;
+} EBox;
+
+typedef struct VexBox
+{
+  VertexType vertex;
+  EBox *firstedge;
+} VexBox;
+
+typedef struct
+{
+  VexBox adjmulist[MAXVERTEXNUM];
+  int vexnum, edgenum;
+} AMLGraph;
 
 #endif
