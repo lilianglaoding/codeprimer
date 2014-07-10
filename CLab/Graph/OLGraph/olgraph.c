9d9897adedaaf3409c76964642a4a17f43f74dcb
lilianglaoding <liliang8904@126.com>
2014-07-10 22:58:04 +0800

    Orthogonal List

--- CLab/Graph/OLGraph/olgraph.c
+++ CLab/Graph/OLGraph/olgraph.c
@@ -3,3 +3,22 @@
 
 #include "olgraph.h"
 
+void CreateOLGraph(OLGraph *G)
+{
+  int i, j, k;
+  char v1, v2;
+  VexNode *s;
+  printf("Please input the vexnum and arcnum(format is (i,j)):\n");
+  scanf("%d,%d", &(G->vexnum), &(G->arcnum));
+  printf("Please input the vertex:\n");
+  for (i = 0; i < G->vexnum; i++)
+  {
+    scanf("%c", &(G->xlixt[i].vertex));
+    xlist[i].firstin = NULL;
+    xlist[i].firstout = NULL;
+  }
+  printf("Please input the vertex(format is <vi,vj>):\n");
+  i = locate(G, vi);
+  j = locate(G, vj);
+  
+}
