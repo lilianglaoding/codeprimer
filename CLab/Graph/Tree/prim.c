0f5fba76da15b9a5052671b65264ab860c50afef
lilianglaoding <liliang8904@126.com>
2014-07-16 23:26:05 +0800

    prime

--- CLab/Graph/Tree/prim.c
+++ CLab/Graph/Tree/prim.c
@@ -3,4 +3,39 @@
 
 #include "mgraph.h"
 
+#define MAXCOST 65535
 
+void prim(int gm[][MAXNODE], int n, int closevertex[])
+{
+  int i, j, k;
+  int lowcost[MAXNODE], mincost;
+  for (i = 1; i < n; i++)
+  {
+    lowcost[i] = gm[0][i];
+    closevertex[i] = 0;
+  }
+  lowcost[0] = 0;
+  closevertex[0] = 0;
+  for (i = 1; i < n; i++)
+  {
+    mincost = MAXCOST;
+    j = 1;
+    k = 0;
+    while (j < n)
+    {
+      if (lowcost[j] != 0 && lowcost[j] < mincost)
+      {
+	mincost = lowcost[j];
+	k = j;
+      }
+      j++;
+    }
+    lowcost[k] = 0;
+    for (j = 1; j < n; j++)
+      if (lowcost[j] != 0 && gm[k][j] < lowcost[j])
+      {
+	lowcost[j] = gm[k][j];
+	closevertex[j] = k;
+      }
+  }
+}
