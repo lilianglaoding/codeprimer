3e5fe2a079c13caae9623d901bd3472e93aedb18
lilianglaoding <liliang8904@126.com>
2014-07-21 22:43:00 +0800

    shortest path

--- CLab/Graph/ShortestPath/shortestpath.c
+++ CLab/Graph/ShortestPath/shortestpath.c
@@ -3,8 +3,30 @@
 
 #include "shortestpat.h"
 
+#define INFINITY 65535
+
 void ShortestPath_1(MGraph G, int v0, PathMatrix *P, ShortPathTable *D)
 {
-  int i, w;
+  int i, w, v;
+  int min;
+  bool final[MAXNUM];
+  for (v = 0; v < G.n; v++)
+  {
+    final[v] = false;
+    D[v] = G.edges[v0][v];
+    P[v0] = -1;
+    if (G.edges[v0][v] < INFINITY && v != v0)
+      P[v] = v0;
+    if (D[v] == INFINITY)
+      P[v] = -2;
+  }
+  
+  D[v0] = 0;
+  final[v0] = true;
+  for (i = 1; i < G.n; i++)
+  {
+    min = INFINITY;
+    
+  }
   
 }
