409e996a889a5e15dd8c25722b85b729224b0557
lilianglaoding <liliang8904@126.com>
2014-07-30 07:11:20 +0800

    Critical Path

--- CLab/Graph/CriticalPath/criticalpath.c
+++ CLab/Graph/CriticalPath/criticalpath.c
@@ -5,8 +5,33 @@
 
 int TopologicalOrder(ALGraph G, Stack T)
 {
+  int i, j, k;
   FindInDegree(G, indegree);
-  
+  InitStack(S);
+  count = 0;
+  for (i = 0; i < G.n; i++)
+    ve[i] = 0;
+  for (i = 0; i < G.n; i++)
+    if (indegree[i] == 0)
+      push(s, i);
+  while (!StackEmpty(S))
+  {
+    Pop(S, j);
+    Push(T, j);
+    ++count;
+    for (p = G.adjlist[i].firstedge; p; p = p->next)
+    {
+      k = p->adjvex;
+      if (--indegree[k] == 0)
+	push(S, k);
+      if (ve[j] + *(p->info) > ve[k])
+	ve[k] = ve[j] + *(p->info);
+    }
+  }
+  if (count < G.n)
+    return 0;
+  else
+    return 1;
 }
 
 
