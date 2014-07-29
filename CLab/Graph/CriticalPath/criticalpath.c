11fffa6b25c2640a803864d83ac7782f8924373b
lilianglaoding <liliang8904@126.com>
2014-07-30 07:25:53 +0800

    Critical Path

--- CLab/Graph/CriticalPath/criticalpath.c
+++ CLab/Graph/CriticalPath/criticalpath.c
@@ -37,6 +37,29 @@ int TopologicalOrder(ALGraph G, Stack T)
 
 int CriticalPath(ALGraph G)
 {
+  int i, j, k;
   InitStack(T);
-  
+  if (!TopologicalOrder(G, T))
+    return 0;
+  for (i = 0; i < G.n; i++)
+    vl[i] = ve[l];
+  while (!StackEmpty(T))
+    for (j = Pop(T), p = G.adjlist[j].firstedge; p; p = p->next)
+    {
+      k = p->adjvex;
+      dut = *(p->info);
+      if (vl[k] - dut < vl[j])
+	vl[j] = vl[k] - dut;
+    }
+  for (j = 0; j < G.n; j++)
+    for (p = G.adjlist[j].firstedge; p; p = p->next)
+    {
+      k = p->adjvex;
+      dut = *(p->info);
+      e = ve[j];
+      l = vl[k] - dut;
+      tag = (e == 1)?'*'';';
+      printf("%d, %d, %d, %d, %d, %c", j, k, dut, e, l, tag);
+    }
+  return 1;
 }
