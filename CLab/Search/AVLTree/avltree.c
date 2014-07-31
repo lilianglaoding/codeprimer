fde01dde98e80c4a5656497dcd350b36b5e3dbb2
lilianglaoding <liliang8904@126.com>
2014-08-01 07:44:52 +0800

    AVL Tree

--- CLab/Search/AVLTree/avltree.c
+++ CLab/Search/AVLTree/avltree.c
@@ -3,4 +3,24 @@
 
 #include "avltree.h"
 
+#define LH -1
+#define EH 0
+#define RH 1
 
+void R_Rotate(NodeType **p)
+{
+  NodeType *q;
+  q = (*p)->lchild;
+  (*p)->lchild = q->rchild;
+  q->rchild = *p;
+  *p = q;
+}
+
+void L_Rotate(NodeType **p)
+{
+  NodeType *q;
+  q = (*p)->rchild;
+  *p->rchild = q->lchild;
+  q->lchild = *p;
+  *p = q;
+}
