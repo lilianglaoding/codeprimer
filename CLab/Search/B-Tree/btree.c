2f3c50006cf9c094ae7ecb05946e2e6ce81082f7
lilianglaoding <liliang8904@126.com>
2014-08-05 22:51:17 +0800

    B- Tree

--- CLab/Search/B-Tree/btree.c
+++ CLab/Search/B-Tree/btree.c
@@ -3,3 +3,27 @@
 
 #include "btree.h"
 
+Result SearchBTree(NodeType **t, KeyType kx)
+{
+  Result rs;
+  NodeType *p = t;
+  NodeType *q = NULL;
+  int i, found = 0;
+  while (p && !found)
+  {
+    i = Search(p, kx);
+    if (i > 0 && p->key[i] == kx)
+      found = 1;
+    else
+    {
+      q = p;
+      p = p->nptr[i];
+    }
+  }
+  rs.tag = found;
+  if (!found)
+    p = q;
+  rs.pt = p;
+  rs.i = i;
+  return rs;
+}
