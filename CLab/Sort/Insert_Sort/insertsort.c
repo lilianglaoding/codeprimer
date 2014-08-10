6a00ba06609da89d330c3329b46240f400b2eb14
lilianglaoding <liliang8904@126.com>
2014-08-10 21:22:05 +0800

    Insert Sort

--- CLab/Sort/Insert_Sort/insertsort.c
+++ CLab/Sort/Insert_Sort/insertsort.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int insertsort(int *array, int size)
+typedef int datatype;
+
+void D-InsertSort(datatype R[], int n)
+{
+  int i, j;
+  for (i = 1; i < n; i++)
+  {
+    if (R[i] < R[i - 1])
+    {
+      for (j = i - 1; j >= 0 && R[i] > R[j]; j--)
+	R[j + 1] = R[j];
+      R[j + 1] = R[i];
+    }
+  }
+}
+
+void print(datatype R[], int n)
 {
   int i;
-  for (i = 0; i < size; i++)
-  {
-    for (i = 1;)
-  }
+  for (i = 0; i < n; i++)
+    printf("%d ", R[i]);
+}
+
+int main()
+{
+  int i;
+  int array[10];
+  for (i = 9; i >= 0; i--)
+    array[i] = i;
+  print(array, 10);
+  printf("\n");
+  D-InsertSort(array, 10);
+  for (i = 0; i < 10; i++)
+    printf("%d ", array[i]);
+  printf("\n");
+  return 0;
 }
