63b9d01f3776fd9a277689d3bde7ddf75a366566
lilianglaoding <liliang8904@126.com>
2014-08-11 07:27:37 +0800

    Table Insert Sort

--- CLab/Sort/Table_Sort/tables_sort.c
+++ CLab/Sort/Table_Sort/tables_sort.c
@@ -5,5 +5,20 @@
 
 void B_InsertSort(datatype R[], int n)
 {
-  
+  int i, j;
+  int p;
+  j = R[0].next = 1;
+  R[1].next = 0;
+  for (i = 2; i < n; i++)
+  {
+    for (j = 0; j < i; j++)
+    {
+      if (R[i] < R[j + 1])
+      {
+	p = R[i].next;
+	R[j].next = R[i].next;
+	R[i].next = j;
+      }
+    }
+  }
 }
