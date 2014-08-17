b2bc9a57e36dbabe067caeded50bbbd7a0101427
lilianglaoding <liliang8904@126.com>
2014-08-18 07:24:54 +0800

    Heap Sort

--- CLab/Sort/Heap_Sort/heap_sort.c
+++ CLab/Sort/Heap_Sort/heap_sort.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void 
+#include "heap_sort.h"
+
+void HeapAdjust(int R[], int s, int t)
+{
+  int rs = R[s];
+  int i = s, j;
+  int m = t - 1;
+  for (j = s; j < t; j = j * 2)
+  {
+    if (j < m && R[j] < R[j + 1])
+      j = j + 1;
+    if (rs > R[j])
+      break;
+    R[i] = R[j];
+    i = j;
+  }
+  R[i] = rs;
+}
