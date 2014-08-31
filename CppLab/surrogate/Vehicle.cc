3bfedf5d6591c97c4495f6bdb42dc25e09b3a105
lilianglaoding <liliang8904@126.com>
2014-08-31 22:38:43 +0800

    surrogate

--- CppLab/surrogate/Vehicle.cc
+++ CppLab/surrogate/Vehicle.cc
@@ -7,4 +7,7 @@ Vehicle parking_lot[1000]; //error, Vehicle can not be instanced
 
 Vehicle *parking_lot1[1000];
 
+AutoVehicle x = /****/;
+
+
 
--- CppLab/surrogate/Vehicle.h
+++ CppLab/surrogate/Vehicle.h
@@ -3,6 +3,8 @@ class Vehicle
  public:
   virtual double weight() const = 0;
   virtual void start() = 0;
+  virtual Vehicle *copy() = 0;
+  virtual ~Vehicle() { };
 };
 
 class RoadVehicle: public Vehicle
