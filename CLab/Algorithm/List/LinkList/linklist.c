eacf5c72a5a282448f6407287f7d8e0f436080c7
lilianglaoding <liliang8904@126.com>
2014-09-03 22:37:19 +0800

    LinkList

--- CLab/Algorithm/List/LinkList/linklist.c
+++ CLab/Algorithm/List/LinkList/linklist.c
@@ -50,5 +50,48 @@ int Create_LinkList1(LinkList L)
     p = (LNode *)malloc(sizeof(LNode));
     if (p == NULL)
       return 0;
+    p->data = x;
+    p->next = NULL;
+    rear->next = p;
+    rear = p;
+    scanf("%d", &x);
+  }
+  return 1;
+}
+
+int Length_LinkList(LinkList L)
+{
+  if (L == NULL)
+    return 0;
+  int len = 0;
+  LinkList p = L->next;
+  while (p)
+  {
+    len++;
+    p = p->next;
+  }
+  return len;
+}
+
+LinkList Get_LinkList(LinkList L, datatype x)
+{
+  if (L == NULL)
+    return NULL;
+  LinkList p;
+  p = L->next;
+  while (p != NULL && p->data != x)
+    p = p->next;
+  return p;
+}
+
+LinkList Locate_LinkList(LinkList L, int i)
+{
+  if (L == NULL)
+    return NULL;
+  int j = 1;
+  LinkList p = L->next;
+  while (j < i && p != NULL)
+  {
+    
   }
 }
