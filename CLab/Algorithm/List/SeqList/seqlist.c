e109fe0cf5f512647562cde779e723a0387f2258
lilianglaoding <liliang8904@126.com>
2014-09-03 21:47:20 +0800

    SeqList

--- CLab/Algorithm/List/SeqList/seqlist.c
+++ CLab/Algorithm/List/SeqList/seqlist.c
@@ -7,7 +7,7 @@ SeqList *Init_SeqList()
 {
   SeqList *L;
   if (!(L = (SeqList *)malloc(sizeof(SeqList))))
-    reuturn NULL;
+    return NULL;
   L->last = -1;
   return L;
 }
@@ -21,7 +21,7 @@ int Insert_SeqList(SeqList *L, int loc, datatype x)
     printf("List pointer is NULL!\n");
     return 0;
   }
-  if (L->last = MAXSIZE - 1)
+  if (L->last == MAXSIZE - 1)
   {
     printf("The List is full!\n");
     return 0;
@@ -44,5 +44,66 @@ int Insert_SeqList(SeqList *L, int loc, datatype x)
 // 0----failure
 int Delete_SeqList(SeqList *L, int loc, datatype *x)
 {
+  if (L == NULL)
+  {
+    printf("List pointer is NULL!\n");
+    return 0;
+  }
+  if (L->last == -1)
+  {
+    printf("List is empty!\n");
+    return 0;
+  }
+  if (loc < 0 || loc > L->last)
+  {
+    printf("Wrong delete location!\n");
+    return 0;
+  }
   
+  int j;
+  *x = L->data[loc];
+  for (j = loc; j < L->last; j++)
+    L->data[j] = L->data[j + 1];
+  L->last--;
+  return 1;
+}
+
+// if found, return the location
+// else, return -1;
+int Search_SeqList(SeqList *L, datatype x)
+{
+  if (L == NULL)
+  {
+    printf("List pointer is empty!\n");
+    return -1;
+  }
+  int i = 0;
+  while (i <= L->last && L->data[i] != x)
+    i++;
+  if (i > L->last)
+    return -1;
+  else
+    return i;
+}
+
+int Destroy_SeqList(SeqList *L)
+{
+  if (L == NULL)
+    return 0;
+  else
+    free(L);
+  return 1;
+}
+
+int main()
+{
+  SeqList *L;
+  L = Init_SeqList();
+  int x = 5;
+  Insert_SeqList(L, 0, x);
+  Delete_SeqList(L, 0, &x);
+  Insert_SeqList(L, 0, x);
+  int i = Search_SeqList(L, x);
+  printf("%d\n", i);
+  Destroy_SeqList(L);
 }
