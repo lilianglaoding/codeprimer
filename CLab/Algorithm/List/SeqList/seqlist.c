4460f12c36f6715add8af968228f49748961cd7b
lilianglaoding <liliang8904@126.com>
2014-09-03 21:29:25 +0800

    SeqList

--- CLab/Algorithm/List/SeqList/seqlist.c
+++ CLab/Algorithm/List/SeqList/seqlist.c
@@ -14,9 +14,35 @@ SeqList *Init_SeqList()
 
 // 1----insert success
 // 0----insert failure
-int Insert_SeqList(SeqList *L, datatype x)
+int Insert_SeqList(SeqList *L, int loc, datatype x)
 {
   if (L == NULL)
+  {
+    printf("List pointer is NULL!\n");
     return 0;
+  }
+  if (L->last = MAXSIZE - 1)
+  {
+    printf("The List is full!\n");
+    return 0;
+  }
+  if (loc < 0 || loc > L->last + 1)
+  {
+    printf("Wrong insert location!\n");
+    return 0;
+  }
+  
+  int j;
+  for (j = L->last; j >= loc; j--)
+    L->data[j + 1] = L->data[j];
+  L->data[loc] = x;
+  L->last++;
+  return 1;
+}
+
+// 1----success
+// 0----failure
+int Delete_SeqList(SeqList *L, int loc, datatype *x)
+{
   
 }
