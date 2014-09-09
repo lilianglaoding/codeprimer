8cc9ce654dee05719a7919ccaeaf61417497cd0e
lilianglaoding <liliang8904@126.com>
2014-09-10 07:37:58 +0800

    Link Stack

--- CLab/Algorithm/Stack/LinkStack/linkstack.c
+++ CLab/Algorithm/Stack/LinkStack/linkstack.c
@@ -8,7 +8,51 @@ LinkStack *Init_Stack()
   return NULL;
 }
 
-LinkList *Pop_LinkList(LinkList *s)
+int Empty_Stack(LinkStack *top)
 {
+  if (top == NULL)
+    return 1;
+  else
+    return 0;
+}
+
+LinkStack *Pop_Stack(LinkStack *top)
+{
+  if (Empty_Stack(top))
+    return NULL;
+  LinkStack q = top;
+  top = top->next;
+  free(q);
+  return top;
+}
+
+LinkStack *Push_Stack(LinkStack *top, datatype x)
+{
+  LinkStack *s;
+  s = (LinkStack *)malloc(sizeof(LinkStack));
+  if (s == NULL)
+    return top;
+  s->data = x;
+  s->next = top;
+  top = s;
+  return top;
+}
+
+void Destroy_Stack(LinkStack *top)
+{
+  LinkStack *p;
+  while (p)
+  {
+    p = top;
+    top = top->next;
+    free(p);
+  }
+}
+
+void Print_Stack(LinkStack *top)
+{
+  if (top == NULL)
+    return;
+  LinkStack *p = top;
   
 }
