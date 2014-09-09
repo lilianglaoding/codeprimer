da7d78c913800ff7c32944cef06717b5344d9999
lilianglaoding <liliang8904@126.com>
2014-09-10 07:43:06 +0800

    Link Stack

--- CLab/Algorithm/Stack/LinkStack/linkstack.c
+++ CLab/Algorithm/Stack/LinkStack/linkstack.c
@@ -20,10 +20,17 @@ LinkStack *Pop_Stack(LinkStack *top)
 {
   if (Empty_Stack(top))
     return NULL;
-  LinkStack q = top;
+  LinkStack *q = top;
   top = top->next;
   free(q);
   return top;
+}
+
+datatype Top_Stack(LinkStack *top)
+{
+  if (top == NULL)
+    return 0;
+  return top->data;
 }
 
 LinkStack *Push_Stack(LinkStack *top, datatype x)
@@ -62,3 +69,15 @@ void Print_Stack(LinkStack *top)
   printf("\n");
 }
 
+int main()
+{
+  LinkStack *top;
+  top = Init_Stack();
+  for (int i = 0; i < 6; i++)
+    top = Push_Stack(top, i);
+  Print_Stack(top);
+  top = Pop_Stack(top);
+  Print_Stack(top);
+  Destroy_Stack(top);
+  return 0;
+}
