b3905e05759096b99a57f0ab565b0ef4f4020bd3
lilianglaoding <liliang8904@126.com>
2014-09-11 07:43:12 +0800

    String

--- CLab/Algorithm/String/string.c
+++ CLab/Algorithm/String/string.c
@@ -3,3 +3,24 @@
 
 #include "string.c"
 
+int String_Length(char *str)
+{
+  int len = 0;
+  while (*str != '\0')
+  {
+    len++;
+    str++;
+  }
+  return len;
+}
+
+int String_Compare(char *str1, char *str2)
+{
+  int ret = 0;
+  while (*str1 != '\0' && (ret = *str1 - *str2) == 0)
+  {    
+    str1++;
+    str2++;
+  }
+  
+}
