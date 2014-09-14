94b4c36ceb9ea69fd072d51a916ee904753c164d
lilianglaoding <liliang8904@126.com>
2014-09-14 22:23:40 +0800

    binary tree

--- CLab/Algorithm/Tree/bitree.c
+++ CLab/Algorithm/Tree/bitree.c
@@ -5,5 +5,24 @@
 
 BiTee Init_BiTree()
 {
-  
+  BiTree p;
+  p = (BiTNode *)malloc(sizeof(BiTNode));
+  if (p == NULL)
+    return NULL;
+  p->lchild = NULL;
+  p->rchild = NULL;
+  return p
 }
+
+BiTree Create_BiTree(datatype x, BiTree lbt, BiTree rbt)
+{
+  Biree p;
+  p = (BiTNode *)malloc(sizeof(BiTNode));
+  if (p == NULL)
+    return NULL;
+  p->lchild = lbt;
+  p->rchild = rbt;
+  p->data = x;
+  return p;
+}
+
