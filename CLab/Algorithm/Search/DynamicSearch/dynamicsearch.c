cab8424025ccdcd5c2ac68b1dc6f510ab19e88bb
lilianglaoding <liliang8904@126.com>
2014-09-24 21:17:37 +0800

    Binary Tee sort search

--- CLab/Algorithm/Search/DynamicSearch/dynamicsearch.c
+++ CLab/Algorithm/Search/DynamicSearch/dynamicsearch.c
@@ -1,5 +1,33 @@
+// Binary Sort Tree
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "dynamicsearch.h"
 
+// *p points to the parent node, *q points to the found node
+int BiTree_Search(NodeType *t, NodeType **p, NodeType **q, datatype kx)
+{
+  int flag = 0;
+  *p = t;
+  *q = *p;
+  while (*q != NULL)
+  {
+    if ((*q)->data < kx)
+    {
+      *p = *q;
+      *q = (*q)->rchild;
+    }
+    else if ((*q)->data > kx)
+    {
+      *p = *q;
+      *q = (*q)->lchild;
+    }
+    else
+    {
+      flag = 1;
+      break;
+    }
+  }
+  return flag;
+}
