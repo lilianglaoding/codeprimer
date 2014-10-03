f76bdfc8e2289ded9ce34a6173f9059b33d8b97f
lilianglaoding <liliang8904@126.com>
2014-10-03 15:29:28 +0800

    AVLTree

--- CLab/Algorithm/Search/DynamicSearch/AVLTree/avltree.c
+++ CLab/Algorithm/Search/DynamicSearch/AVLTree/avltree.c
@@ -3,3 +3,16 @@
 
 #include "avltree.h"
 
+#define LH 1
+#define EH 0
+#define RH -1
+
+void R_Rotate(NodeType **p)
+{
+    Nodetype *lp;
+    lp = (*p)->lchild;
+    (*p)->lchild = lp->rchild;
+    lp->rchild = *p;
+    *p = lp;
+}
+
