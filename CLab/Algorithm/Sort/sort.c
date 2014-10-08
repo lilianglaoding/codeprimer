36774cf33f757dd37214520a73914b2ea66adb1b
lilianglaoding <liliang8904@126.com>
2014-10-08 21:20:35 +0800

    sort

--- CLab/Algorithm/Sort/sort.c
+++ CLab/Algorithm/Sort/sort.c
@@ -5,5 +5,19 @@
 
 void D_InsertSort(datatype *arr, int n)
 {
-    
+    int i = 0, j = 0;
+    datatype tmp;
+    for (i = 1; i < n; i++)
+    {
+	if (arr[i] < arr[i - 1])
+	{
+	    tmp = arr[i];
+	    j = 0;
+	    while (j < i && arr[i] > arr[j])
+		j++;
+	    for (k = i; k > j; k--)
+		arr[k] = arr[k - 1];
+	    arr[k] = tmp;
+	}
+    }
 }
