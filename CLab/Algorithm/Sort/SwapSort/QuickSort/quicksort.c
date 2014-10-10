17ae6dc54831079c7ddca0a5b1ee795c38e36337
lilianglaoding <liliang8904@126.com>
2014-10-11 07:56:34 +0800

    Quick Sort

--- CLab/Algorithm/Sort/SwapSort/QuickSort/quicksort.c
+++ CLab/Algorithm/Sort/SwapSort/QuickSort/quicksort.c
@@ -3,3 +3,37 @@
 
 #include "quicksort.h"
 
+int Partion(datatype *arr, int low, int high)
+{
+    datatype temp;
+    temp = arr[low];
+    while (low < high)
+    {
+	while (low < high && temp < arr[high])
+	    high--;
+	if (low < high)
+	{
+	    arr[low] = arr[high];
+	    low++;
+	}
+	while (low < high && temp > arr[low])
+	    low++;
+	if (low < high)
+	{
+	    arr[high] = arr[low];
+	    high--;
+	}
+    }
+    arr[low] = temp;
+    return low;
+}
+
+void Quick_Sort(datatype *arr, int s, int t)
+{
+    
+}
+
+int main()
+{
+    return 0;
+}
