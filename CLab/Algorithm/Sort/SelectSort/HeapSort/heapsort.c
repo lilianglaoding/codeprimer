31a4fa7d2d20a75e80c04b6be3076356a3af28e2
lilianglaoding <liliang8904@126.com>
2014-10-15 07:37:26 +0800

    heap sort

--- CLab/Algorithm/Sort/SelectSort/HeapSort/heapsort.c
+++ CLab/Algorithm/Sort/SelectSort/HeapSort/heapsort.c
@@ -3,12 +3,21 @@
 
 #include "heapsort.h"
 
+/**************************************
+ k[i] <= {k[2i], k[2i+1]} 
+ or
+ k[i] >= {k[2i], k[2i+1]}
+**************************************/
+
 void Heap_Adjust(datatype *arr, int s, int t)
 {
     
 }
 
-
+void Heap_Sort(datatype *data, int n)
+{
+    
+}
 
 int main()
 {
