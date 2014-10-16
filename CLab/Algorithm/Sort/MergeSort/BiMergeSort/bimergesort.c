8c23fd4864b6aa1fd72c7533a6ec3c996e7c6094
lilianglaoding <liliang8904@126.com>
2014-10-17 07:31:06 +0800

    Merge Sort

--- CLab/Algorithm/Sort/MergeSort/BiMergeSort/bimergesort.c
+++ CLab/Algorithm/Sort/MergeSort/BiMergeSort/bimergesort.c
@@ -24,8 +24,20 @@ void BiMergeSort(datatype *src, datatype *dst, int m, int s, int t)
 	else
 	    dst[k++] = src[j++];
     }
-    while (i < s)
+    while (i <= s)
 	dst[k++] = src[i++];
-    while (j < t)
+    while (j <= t)
 	dst[k++] = src[j++];
+}
+
+int main()
+{
+    datatype src[8] = {1, 2, 3, 4, 2, 3, 4, 5};
+    datatype dst[8] = {0};
+    printf("---------------before sorted-----------------\n");
+    Print(src, 8);
+    BiMergeSort(src, dst, 0, 3, 7);
+    printf("---------------after sorted------------------\n");
+    Print(dst, 8);
+    return 0;
 }
