df6e8122cff436182bc2c6b712d4a2b2a66714df
lilianglaoding <liliang8904@126.com>
2014-10-16 22:14:50 +0800

    Heap Sort

--- CLab/Algorithm/Sort/SelectSort/HeapSort/heapsort.c
+++ CLab/Algorithm/Sort/SelectSort/HeapSort/heapsort.c
@@ -9,7 +9,13 @@
  k[i] >= {k[2i], k[2i+1]}
 **************************************/
 
-void Print(datatype *arr, int n);
+void Print(datatype *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+	printf("%d ", arr[i]);
+    printf("\n");
+}
 
 void Heap_Adjust(datatype *arr, int s, int t)
 {
@@ -17,7 +23,7 @@ void Heap_Adjust(datatype *arr, int s, int t)
     datatype temp;
     i = s;
     temp = arr[s];
-    for (j = 2 * i + 1; j <= t; j = 2 * j)
+    for (j = 2 * i + 1; j < t; j = 2 * j + 1)
     {
 	if (arr[j + 1] > arr[j])
 	    j = j + 1;
@@ -33,36 +39,26 @@ void Heap_Sort(datatype *arr, int n)
 {
     int i;
     datatype temp;
-    for (i = n / 2 - 1; i >= 0; i--)
+    for (i = (n - 1) / 2; i >= 0; i--)
 	Heap_Adjust(arr, i, n - 1);
-    Print(arr, n);
     //sort for small to big, find the biggest and put it in arr[i]
     for (i = n - 1; i >= 0; i--)
     {
-	printf("the %d times: arr[0] is %d\n", i, arr[0]);
 	temp = arr[0];
 	arr[0] = arr[i];
 	arr[i] = temp;
 	Heap_Adjust(arr, 0, i - 1);
-    }
-	
-}
-
-void Print(datatype *arr, int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-	printf("%d ", arr[i]);
-    printf("\n");
+    }	
 }
 
 int main()
 {
     datatype arr[8] = {16, 24, 53, 47, 36, 85, 30, 91};
+    datatype arr1[8] = {8, 7, 6, 5, 4, 3, 2, 1};
     printf("-----------------before sorted---------------\n");
-    Print(arr, 8);
+    Print(arr1, 8);
     printf("-----------------after sorted----------------\n");
-    Heap_Sort(arr, 8);
-    Print(arr, 8);
+    Heap_Sort(arr1, 8);
+    Print(arr1, 8);
     return 0;
 }
