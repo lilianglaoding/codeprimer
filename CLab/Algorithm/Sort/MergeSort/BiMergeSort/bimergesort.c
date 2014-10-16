973a1344025fdd4304fd697445cbcf8fd6a6c041
lilianglaoding <liliang8904@126.com>
2014-10-16 23:29:33 +0800

    Merge Sort

--- CLab/Algorithm/Sort/MergeSort/BiMergeSort/bimergesort.c
+++ CLab/Algorithm/Sort/MergeSort/BiMergeSort/bimergesort.c
@@ -3,4 +3,15 @@
 
 #include "bimergesort.h"
 
+void Print(datatype *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+	printf("%d ", arr[i]);
+    printf("\n");
+}
 
+void BiMergeSort(datatype *src, datatype *dst, int m, int s, int t)
+{
+    
+}
