a48e03e58d636b30d1ad0d5e265910d69bdab5b3
lilianglaoding <liliang8904@126.com>
2014-10-27 21:51:58 +0800

    Radix Sort

--- CLab/Algorithm/Sort/RadixSort/radixsort.c
+++ CLab/Algorithm/Sort/RadixSort/radixsort.c
@@ -5,7 +5,18 @@
 
 void Distribute(NodeType *R, int i, Queue q)
 {
-    
+    int i, p;
+    for (i = 0; i < RADIX; i++)
+	q[i].f = q[i].e = 0;
+    for (p = R[0].next; p; p = R[p].next)
+    {
+	i = R[p].keys[i];
+	if (!q[j].f)
+	    q[j].f = p;
+	else
+	    R[q[j].e].next = p;
+	q[j].e = p;
+    }
 }
 
 void Collect(NodeType *R, int i, Queue q)
