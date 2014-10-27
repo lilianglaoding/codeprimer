8bb48cbe2f94f531b10157125b793ecef3f0f6a5
lilianglaoding <liliang8904@126.com>
2014-10-28 07:28:59 +0800

    Radix Sort

--- CLab/Algorithm/Sort/RadixSort/radixsort.c
+++ CLab/Algorithm/Sort/RadixSort/radixsort.c
@@ -5,27 +5,36 @@
 
 void Distribute(NodeType *R, int i, Queue q)
 {
-    int i, p;
-    for (i = 0; i < RADIX; i++)
-	q[i].f = q[i].e = 0;
+    int j, p;
+    for (j = 0; j < RADIX; j++)
+	q[j].f = q[j].e = 0;
     for (p = R[0].next; p; p = R[p].next)    //R[0] restores the head pointer
     {
-	i = R[p].keys[i] % 10;
-	if (!q[i].f)
-	    q[i].f = p;
+	j = R[p].keys[i] % 10;       // map to [0, RADIX - 1]
+	if (!q[j].f)
+	    q[j].f = p;
 	else
-	    R[q[i].e].next = p;
-	q[i].e = p;
+	    R[q[j].e].next = p;
+	q[j].e = p;
     }
 }
 
 void Collect(NodeType *R, int i, Queue q)
 {
-    int i;
-    for (i = 0; !q[i].f; i = succ(i))
-	R[0].next = q[i].f;
+    int j;
+    for (j = 0; !q[j].f; j = succ(j));
+    R[0].next = q[j].f;
     t = q[i].e;
-    
+    while (j < RADIX)
+    {
+	for (j = succ(j); j < RADIX - 1 && !q[j].f; j = succ(j));
+	if (q[j].f)
+	{
+	    R[t].next = q[j].f;
+	    t = q[j].e;
+	}
+	R[t].next = 0;
+    }
 }
 
 void RadixSort(NodeType *R, int n)
