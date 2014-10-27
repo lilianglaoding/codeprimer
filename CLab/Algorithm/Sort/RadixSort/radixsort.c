f4448e4ac2a00b39a3ef2568067ffefe14629e13
lilianglaoding <liliang8904@126.com>
2014-10-27 22:22:37 +0800

    Radix Sort

--- CLab/Algorithm/Sort/RadixSort/radixsort.c
+++ CLab/Algorithm/Sort/RadixSort/radixsort.c
@@ -8,25 +8,38 @@ void Distribute(NodeType *R, int i, Queue q)
     int i, p;
     for (i = 0; i < RADIX; i++)
 	q[i].f = q[i].e = 0;
-    for (p = R[0].next; p; p = R[p].next)
+    for (p = R[0].next; p; p = R[p].next)    //R[0] restores the head pointer
     {
 	i = R[p].keys[i];
-	if (!q[j].f)
-	    q[j].f = p;
+	if (!q[i].f)
+	    q[i].f = p;
 	else
-	    R[q[j].e].next = p;
-	q[j].e = p;
+	    R[q[i].e].next = p;
+	q[i].e = p;
     }
 }
 
 void Collect(NodeType *R, int i, Queue q)
 {
+    int i;
+    for (i = 0; !q[i].f; i = succ(i))
+	R[0].next = q[i].f;
+    t = q[i].e;
     
 }
 
 void RadixSort(NodeType *R, int n)
 {
-    
+    int i;
+    Queue q;
+    for (i = 0; i < n; i++)
+	R[i].next = i + 1;
+    R[i].next = 0;
+    for (i = 0; i < KEY_NUM; i++)
+    {
+	Distribute(R, i, q);
+	Collect(R, i, q);
+    }
 }
 
 int main()
