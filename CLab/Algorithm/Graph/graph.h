4f612ffcfd7c58f3542040bdce3bf58d98cb2c6f
lilianglaoding <liliang8904@126.com>
2014-11-19 21:23:42 +0800

    Graph

--- CLab/Algorithm/Graph/graph.h
+++ CLab/Algorithm/Graph/graph.h
@@ -1,1 +1,35 @@
-typedef int datatype;
+#define MAXVERTEXNUM 100
+
+typedef char VertexType;
+typedef int EdgeType;
+
+typedef struct
+{
+    VertexType vexs[MAXVERTEXNUM];
+    EdgeType edges[MAXVERTEXNUM][MAXVERTEXNUM];
+    int n, e;
+} MGraph;
+
+
+typedef struct node
+{
+    int adjvex;
+    struct node *next;
+} EdgeNode;
+
+typedef struct vnode
+{
+    VertexType vertex;
+    EdgeNode *firstedge;
+} VertexNode;
+
+typedef VertexNode AdjList[MAXVERTEXNUM];
+
+typedef struct
+{
+    AdjList adjlist;
+    int n, e;
+} ALGraph;
+
+
+
