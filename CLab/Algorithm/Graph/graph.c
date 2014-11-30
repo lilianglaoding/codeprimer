0ffad43c93e99773f75d253606ce334c190ca2dc
lilianglaoding <liliang8904@126.com>
2014-12-01 07:40:35 +0800

    Graph

--- CLab/Algorithm/Graph/graph.c
+++ CLab/Algorithm/Graph/graph.c
@@ -2,9 +2,30 @@
 #include <stdlib.h>
 #include "graph.h"
 
-void DFSAL(ALGraph *G)
+void DFSAL(ALGraph *G, int i);
+
+void DFSTraverseAL(ALGraph *G)
 {
+    int visited[G->n];
     int i;
+    for (i = 0; i < G->n; i++)
+	visited[i] = 0;
+    for (i = 0; i < G->n; i++)
+	DFSAL(G, i);
+}
+
+void DFSAL(ALGraph *G, int i)
+{
+    EdgeNode *p;
+    printf("visit vertex: V%c\n", G->adjlist[i].vertex);
+    visited[i] = 1;
+    p = G->adjlist[i].firstedge;
+    while (p)
+    {
+	if (!visited[p->adjvex])
+	    DFSAL(G, p->adjvex);
+	p = p->next;
+    }
 }
 
 int main()
