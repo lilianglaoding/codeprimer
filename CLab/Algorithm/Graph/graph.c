b27d1b7cfb8f64b7986d0583f792058f05a2ff73
lilianglaoding <liliang8904@126.com>
2014-12-01 07:31:16 +0800

    Graph

--- CLab/Algorithm/Graph/graph.c
+++ CLab/Algorithm/Graph/graph.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 #include "graph.h"
 
-void CreateMGraph(MGraph *G)
-{
-    int i, j, k, w;
-    char ch;
-    for (i = 0; i < G->n; i++)
-	scanf("%d", &G->vex[i]);
-}
-
 void DFSAL(ALGraph *G)
 {
     int i;
