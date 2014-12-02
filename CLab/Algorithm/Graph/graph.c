a854b9dd431026e2d6170e9796943f3565c8d0bc
lilianglaoding <liliang8904@126.com>
2014-12-03 07:35:57 +0800

    Graph

--- CLab/Algorithm/Graph/graph.c
+++ CLab/Algorithm/Graph/graph.c
@@ -7,11 +7,15 @@ void DFSAL(ALGraph *G, int i);
 void DFSTraverseAL(ALGraph *G)
 {
     int i;
+    int count = 0;
     for (i = 0; i < G->n; i++)
 	G->visited[i] = 0;
     for (i = 0; i < G->n; i++)
 	if (!G->visited[i])
+	{
 	    DFSAL(G, i);
+	    count++;  // to test if the graph is all connected
+	}
 }
 
 void DFSAL(ALGraph *G, int i)
@@ -33,11 +37,15 @@ void BFSM(MGraph *G, int k);
 void BFSTraverseM(MGraph *G)
 {
     int i;
+    int count = 0;
     for (i = 0; i < G->n; i++)
 	G->visit[i] = 0;
     for (i = 0; i < G->n; i++)
 	if (!G->visited[i])
+	{
 	    BFSM(G, i);
+	    count++;        // to test if the graph is all connected
+	}
 }
 
 void BFSM(MGraph *G, int k)
