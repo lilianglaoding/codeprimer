e743899b3a676c4750ca0c0f83137cce4505f6d9
lilianglaoding <liliang8904@126.com>
2014-12-26 22:18:22 +0800

    ip address

--- RubyLab/ipaddr.rb
+++ RubyLab/ipaddr.rb
@@ -1,6 +1,6 @@
-for i in 1..255
-  1stbyte = i % 256
-  2ndbyte = i / 256
-  puts "192.#{2ndbyte}.#{1stbyte}.1"
+for i in 1..1024
+  stbyte = i % 255
+  ndbyte = i / 255
+  puts "192.#{ndbyte}.#{stbyte}.1"
 end
 		
