661d6d8c6852551443e5305c4715ca1c0857b21d
lilianglaoding <liliang8904@126.com>
2015-01-05 20:35:41 +0800

    cli

--- RubyLab/cli.rb
+++ RubyLab/cli.rb
@@ -1,19 +1,21 @@
-cli_all = ""
+cli_all = "test
+aaa
+bbb
+ccc"
 cli_need_cut = "test
 aaa
-bbb"
+bbb
+"
 cli_no_need_cut = ""
 
 def str_equal(str, substr)
   if (str == substr)
-    break;
-  else
-    break
+    puts substr
   end
 end
 
 def cut_off(str, substr)
-  str.each_line {|s| str_equal_str(str, substr)}
+  str.each_line {|s| str_equal(s, substr)}
 end
 
-cli_need_cut.each_line {|s| cut_off(cli_all, cli_need_cut)}
+cli_need_cut.each_line {|s| cut_off(cli_all, s)}
