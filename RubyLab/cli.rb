817ae734eef5cf7808a7f80a7192e7ff502962d4
lilianglaoding <liliang8904@126.com>
2015-01-05 20:29:55 +0800

    cli

--- RubyLab/cli.rb
+++ RubyLab/cli.rb
@@ -4,8 +4,16 @@ aaa
 bbb"
 cli_no_need_cut = ""
 
-def cut_off(str, substr)
-	puts ""
+def str_equal(str, substr)
+  if (str == substr)
+    break;
+  else
+    break
+  end
 end
 
-cli_need_cut.each_line {|s| puts s}
+def cut_off(str, substr)
+  str.each_line {|s| str_equal_str(str, substr)}
+end
+
+cli_need_cut.each_line {|s| cut_off(cli_all, cli_need_cut)}
