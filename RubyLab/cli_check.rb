13b5c803076c1da160b05daaa14fa0b7c4fae719
lilianglaoding <liliang8904@126.com>
2015-01-08 23:06:48 +0800

    cli check

--- RubyLab/cli_check.rb
+++ RubyLab/cli_check.rb
@@ -1,6 +1,8 @@
 file_cli_all = "all"
-file_cli_prune = "purne"
+file_cli_pruned = "purned"
 file_cli_support = "support"
+
+file_array = [file_cli_all, file_cli_pruned, file_cli_support]
 
 def string_replace(str)
   str.gsub(/<.*?>/, '<1>')
@@ -15,8 +17,11 @@ def file_replace(file)
   end
 end
 
-def file_process(file1, file2, file3)
-  for file in [file]
-    file_replace(file)
+def file_process(file_arr)
+  for file in [file_arr]
+    file_replace(file.to_s)
+  end
 end
+
+file_process(file_array)
 
