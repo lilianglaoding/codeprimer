42b645bf3d70ba5c504ad22c02f1c7cdac641822
lilianglaoding <liliang8904@126.com>
2015-01-08 23:49:07 +0800

    cli check

--- RubyLab/cli_check.rb
+++ RubyLab/cli_check.rb
@@ -3,7 +3,9 @@ file_cli_pruned = "pruned"
 file_cli_support = "support"
 
 file_array = [file_cli_all, file_cli_pruned, file_cli_support]
-#puts file_array
+puts file_array[0]
+puts file_array[1]
+puts file_array[2]
 
 def string_replace(str)
   str.gsub(/<.*?>/, '<1>')
@@ -14,16 +16,34 @@ def file_replace(file)
     str = open(file) {|f| string_replace(f.read)}
     open(file, "w") {|f| f.puts str}
   else
-    puts "File #{file} dosen't exist!"
+    puts "Error! File #{file} dosen't exist!"
   end
 end
 
 def file_process(file_arr)
   for file in file_arr
     file_replace(file)
-    puts file_arr
+    #puts file_arr
   end
 end
 
-file_process(file_array)
+def check_pruned_cli(file_all, file_pruned)
+  cli_all = open(file_all) {|f| f.read}
+  cli_pruned = open(file_pruned) {|f| f.read}
+  puts cli_all
+end
+
+def check_support_cli(file_all, file_support)
+  
+end
+
+def check_cli(file_arr)
+  file_all = file_arr[0]
+  file_pruned = file_arr[1]
+  file_support = file_arr[2]
+  check_pruned_cli(file_all, file_pruned)
+  check_support_cli(file_all, file_support)
+end
+
+check_cli(file_array)
 
