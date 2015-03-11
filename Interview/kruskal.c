a31a773dc3b0fcc2f52f4cb7ba3546273f3a0e79
lilianglaoding <liliang8904@126.com>
2015-03-11 22:33:34 +0800

    test

--- Interview/kruskal.c
+++ Interview/kruskal.c
@@ -5,10 +5,40 @@
 
 struct node
 {
-    int v1, v2;
+    int v1;
+    int v2;
     int cost;
 } EdgeNode;
 
 // edges is ordered by cost
 EdgeNode edges[MAXEDGE];
 
+int Find(int farther[], int i)
+{
+    int v = i;
+    while (v >= 0)
+	v = farther[v];
+    return v;
+}
+
+void kruskal(EdgeType edges[], int n)
+{
+    int i, j, vf1, vf2;
+    int farther[MAXEDGE];
+    for (i = 0; i < MAXEDGE; i++)
+	farther[i] = -1;
+    i = 0;
+    j = 0;
+    while (i < MAXEDGE && j < n - 1)
+    {
+	vf1 = Find(farther, edges[i].v1);
+	vf2 = Find(farther, edges[i].v2);
+	if (vf1 != vf2)
+	{
+	    farther[vf2] = vf1;
+	    j++;
+	    printf("%d, %d", edges[i].v1, edges[i].v2);
+	}
+	i++;
+    }
+}
