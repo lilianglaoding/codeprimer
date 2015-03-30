e0f36701c2403fcda89c5300642c5a47530133c4
lilianglaoding <liliang8904@126.com>
2015-03-30 23:11:37 +0800

    list add

---
+++ CLab/C_Prog/listadd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *plast;
+    struct node *pnext;
+} NodeType;
+
+
+
+int main()
+{
+    
+    return 0;
+}
