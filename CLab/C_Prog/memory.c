10a65f23274ddbd38f338d65f4e304abecbfe438
lilianglaoding <liliang8904@126.com>
2015-04-09 23:02:42 +0800

    memory

---
+++ CLab/C_Prog/memory.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MEM_BLK_SIZE 16
+
+void *InitMemPool(size_t mem_pool_size)
+{
+    return NULL;
+}
+
+void DestroyMemPool(void)
+{
+    
+}
+
+void *MemAlloc(size_t user_blk_size)
+{
+    
+}
+
+int MemFree(void *mem_addr)
+{
+    
+}
