b96b8559937af738aabd29fd6846ca2bb4947fdd
lilianglaoding <liliang8904@126.com>
2015-12-06 22:49:30 +0800

    test

--- CLab/C_Prog/test4.c
+++ CLab/C_Prog/test4.c
@@ -3,7 +3,17 @@
 
 char *GetValueByKey(char *key, keyval *keyvalue, int keynum)
 {
-  if (aaa)
+    for (int i = 0; i < 5; i++)
     {
-  }
+        return NULL;
+    }
+    if (a = 1)
+    {
+        return NULL;
+    }
+}
+
+int main()
+{
+  char *test = GetValueByKey(a, b, c);
 }
