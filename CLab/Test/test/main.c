3f6dd42adc1f7c042a54a237acd8f229e24f9caa
lilianglaoding <liliang8904@126.com>
2016-03-31 22:34:53 +0800

    test

--- CLab/Test/test/main.c
+++ CLab/Test/test/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct std
+{
+    char *name;
+    int age;
+    int class;
+    int grade;
+} student;
+
 int main()
 {
+    student stu;
+    int a;
+    stu.name = "liliang";
+    stu.age = 15;
+    stu.grade = 4;
+    stu.class = 3;        
     printf("Hello world!\n");
+    printf("this is test program");
     return 0;
 }
---
+++ CLab/Test/test/test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linklist
+{
+    int data;
+    struct linklist *next;
+} * LinkList;
+
+int main()
+{
+    int a = 0;
+    LinkList list;
+    printf("Hello, world!\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
